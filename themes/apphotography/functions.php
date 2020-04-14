9492d9d34a68179e6c63011db27c90e70e014e0c
Emily <lam.emily813@gmail.com>
2020-04-14 10:12:55 -0400

    Removed code related to removed underscores files

--- themes/apphotography/functions.php
+++ themes/apphotography/functions.php
@@ -144,11 +144,6 @@ add_action( 'widgets_init', 'apphotography_widgets_init' );
  */
 function apphotography_scripts() {
 	wp_enqueue_style( 'apphotography-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'apphotography-style', 'rtl', 'replace' );
-
-	wp_enqueue_script( 'apphotography-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	wp_enqueue_script( 'apphotography-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -170,10 +165,6 @@ function apphotography_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'apphotography_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.

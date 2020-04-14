ac180c531108698120972ef7edb01f3179412464
Emily <lam.emily813@gmail.com>
2020-04-14 12:12:12 -0400

    Rearranged stylesheet enqueues

--- themes/apphotography/functions.php
+++ themes/apphotography/functions.php
@@ -176,15 +176,12 @@ add_action( 'widgets_init', 'apphotography_widgets_init' );
  */
 function apphotography_scripts() {
 
-		
+	// gets style.css file
+	wp_enqueue_style( 'apphotography-style', get_stylesheet_uri(), array() );
+
 	// Reset css stylesheet
 	wp_enqueue_style('apphotography-reset',get_template_directory_uri() . '/assets/css/vendors/reset.css',  array());
 	
-	// Reset css stylesheet
-	wp_enqueue_style('apphotography-reset',get_template_directory_uri() . 'styleZ.css',  array());
-	
-	// gets style.css file
-	wp_enqueue_style( 'apphotography-style', get_stylesheet_uri(), array() );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -192,9 +189,13 @@ function apphotography_scripts() {
 
 	// enqueue foundation styles
 	wp_enqueue_style('apphotography-foundation',get_template_directory_uri() . '/assets/css/vendors/foundation.min.css', null, '6.5.1');
-	wp_enqueue_style('apphotography-apphotographyStyles',get_template_directory_uri() . '/assets/css/apphotography.css', array());
+	wp_enqueue_style('apphotography-foundationStyles',get_template_directory_uri() . '/assets/css/app.css',  array());
 
-	wp_enqueue_style('apphotography-reset',get_template_directory_uri() . '/assets/css/app.css',  array());
+	// Theme css stylesheet
+	wp_enqueue_style('apphotography-apphotographyStyles',get_template_directory_uri() . '/assets/css/apphotography.css', array());
+	
+	// StyleZ css stylesheet
+	wp_enqueue_style('apphotography-styleZ',get_template_directory_uri() . 'styleZ.css',  array());
 
 	// adding what-input js
 	wp_enqueue_script( 'apphotography-what-input', get_template_directory_uri() . '/assets/js/vendors/what-input.js', array('jquery'), '6.5.1', true );

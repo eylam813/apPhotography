e638ad6b740f89ddad399d4584129543aa1bf69b
Emily <lam.emily813@gmail.com>
2020-04-15 11:48:34 -0400

    attempts to alter left-feature page layout with foundations

--- themes/apphotography/page-templates/left-feature-image.php
+++ themes/apphotography/page-templates/left-feature-image.php
@@ -14,29 +14,40 @@
  * @package apPhotography
  */
 
-get_header();
+
 ?>
+    <section class="grid-x ap-whole-page">
+        <!-- header -->
+        <header class="large-3 medium-3 small-12">
+            <?php
+                get_header();
+            ?>
+        </header>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-            <div class="grid-container">
-                <?php
-                while ( have_posts() ) :
-                    the_post();
+        <section class="ap-main-content-wrapper large-9 medium-9 small-12">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <?php
+                    while ( have_posts() ) :
+                        the_post();
 
-                    get_template_part( 'template-parts/content', 'page' );
+                        get_template_part( 'template-parts/content', 'page' );
 
-                    // If comments are open or we have at least one comment, load up the comment template.
-                    if ( comments_open() || get_comments_number() ) :
-                        comments_template();
-                    endif;
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif;
 
-                endwhile; // End of the loop.
-                ?>
-            </div> <!-- grid-container-->
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                    endwhile; // End of the loop.
+                    ?>
+                </main><!-- #main -->
+            </div><!-- #primary -->
+
+            <?php
+            get_sidebar();
+            get_footer();
+            ?>
+        </section> <!--.ap-main-content-wrapper-->
+    </section> <!--whole page-->
 
 <?php
-get_sidebar();
-get_footer();

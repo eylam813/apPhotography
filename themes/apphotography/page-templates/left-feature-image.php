d78ffbc4773971e1eb40b7c3dcf70d0fe8c1e39b
Emily <lam.emily813@gmail.com>
2020-04-15 12:05:43 -0400

    adjusting grid classes in custom left-feature template

--- themes/apphotography/page-templates/left-feature-image.php
+++ themes/apphotography/page-templates/left-feature-image.php
@@ -16,38 +16,43 @@
 
 
 ?>
-    <section class="grid-x ap-whole-page">
+    <!-- <section class="grid-x ap-whole-page"> -->
         <!-- header -->
-        <header class="large-3 medium-3 small-12">
+        <header class="large-3 medium-3 small-12 grid-x">
             <?php
                 get_header();
             ?>
         </header>
 
-        <section class="ap-main-content-wrapper large-9 medium-9 small-12">
-            <div id="primary" class="content-area">
-                <main id="main" class="site-main">
-                    <?php
-                    while ( have_posts() ) :
-                        the_post();
+        <!-- <section class="ap-main-content-wrapper large-9 medium-9 small-12 grid-x"> -->
+            
+                <div id="primary" class="content-area">
+                    <div class="grid-x">
+                        <main id="main" class="site-main ap-main-content-wrapper large-9 medium-9 small-12 grid-x">
+                            <?php
+                            while ( have_posts() ) :
+                                the_post();
 
-                        get_template_part( 'template-parts/content', 'page' );
+                                get_template_part( 'template-parts/content', 'page' );
 
-                        // If comments are open or we have at least one comment, load up the comment template.
-                        if ( comments_open() || get_comments_number() ) :
-                            comments_template();
-                        endif;
+                                // If comments are open or we have at least one comment, load up the comment template.
+                                if ( comments_open() || get_comments_number() ) :
+                                    comments_template();
+                                endif;
 
-                    endwhile; // End of the loop.
-                    ?>
-                </main><!-- #main -->
-            </div><!-- #primary -->
+                            endwhile; // End of the loop.
+                            ?>
+                        </main><!-- #main -->
+                    </div>  <!--.ap-main-content-wrapper-->
+                </div><!-- #primary -->
 
-            <?php
-            get_sidebar();
-            get_footer();
-            ?>
-        </section> <!--.ap-main-content-wrapper-->
+                <?php
+                get_sidebar();
+                get_footer();
+                ?>
+            
+        <!-- </section>  -->
+        <!--.ap-main-content-wrapper-->
     </section> <!--whole page-->
 
 <?php

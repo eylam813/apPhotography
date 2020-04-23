1838e6308f32366e34dedeb6c9e62fe457578c0b
Emily <lam.emily813@gmail.com>
2020-04-23 12:59:37 -0400

    Remove front-page background, and set title to display none. Leaving it there for accessibility reasons.

--- themes/apphotography/front-page.php
+++ themes/apphotography/front-page.php
@@ -19,19 +19,11 @@ get_header();
                 if ( have_posts() ) : 
                     while ( have_posts() ) : the_post();
                         ?> <h1> <?php the_title(); ?> </h1> <?php
-                        // the_title( '&lt;h1>', '&lt;/h1>' );
                         the_content();
                     endwhile;
-                    
-                    // If comments are open or we have at least one comment, load up the comment template.
-                    if ( comments_open() || get_comments_number() ) :
-                        comments_template();
-                    endif;
                 endif; 
                 wp_reset_query()
             ?>
-
-            
             </main><!-- #main -->
         </div>
         <?php get_footer(); ?>

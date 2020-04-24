c219a8e5552d35791ec6d43e4f17630f3abd6e35
Emily <lam.emily813@gmail.com>
2020-04-24 11:06:07 -0400

    Fix styles and remove title output from front-page template

--- themes/apphotography/front-page.php
+++ themes/apphotography/front-page.php
@@ -18,7 +18,6 @@ get_header();
             <?php 
                 if ( have_posts() ) : 
                     while ( have_posts() ) : the_post();
-                        ?> <h1> <?php the_title(); ?> </h1> <?php
                         the_content();
                     endwhile;
                 endif; 

d5f2989b2f69a1039ceb4195c4c6524d43076112
Emily <lam.emily813@gmail.com>
2020-05-01 22:49:28 -0400

    Gallery page inital fix

--- themes/apphotography/page-gallery.php
+++ themes/apphotography/page-gallery.php
@@ -12,19 +12,12 @@
 get_header();
 ?>
     <section class="grid-x ap-whole-page large-12 medium-12 small-12">
-        <!-- header -->
-        <!-- <header class="large-3 medium-3 small-12 grid-x"> -->
-            <?php
-                
-            ?>
-        <!-- </header> -->
-
-        <!-- <section class="ap-main-content-wrapper large-9 medium-9 small-12 grid-x"> -->
                 
         <div id="gallery-ap-primary" class=" gallery-template gallery-ap-content-area grid-x ">
             <div class="gallery-content grid-x large-12 meduim-12 small-12">
                 <div id="gallery-title-wrapper" class="cell"><h3>Gallery</h3></div>
-                <div id="gallery-albums-wrapper" class="cell large-12 medium-12 small-12">
+                <div id="gallery-albums-wrapper" class="cell">
+                    <div id="gallery-albums-inner-wrapper" class="cell grid-x">
                     <?php 
                     $args = array (
                         'post_type' => 'apphoto_album',
@@ -38,16 +31,19 @@ get_header();
                         while ($events->have_posts()) :
                             $events->the_post();
                     ?>
-                        <a class="single-album-wrapper" href="<?php echo get_permalink() ?>">
-                            <div class="single-album-inner-wrapper cell large-auto" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)">
-                                <h2 class="large-12 album-title"><?php the_title(); ?></h2>
+                        <a class="single-album-wrapper large-4 medium-4 small-10 " href="<?php echo get_permalink() ?>">
+                            <div class="single-album-inner-wrapper">
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>">
                             </div>
+                            <h2 class="large-12 album-title"><?php the_title(); ?></h2>
                         </a>
+                        
                         <?php endwhile; ?>
                         <!-- end of loop -->
                         <?php else : ?>
                             <p><?php esc_html__('No Photo Albums at the moment') ?></p>
                         <?php endif; ?>
+                    <div> <!-- #gallery-albums-inner-wrapper -->
                 </div> <!--#gallery-albums-wrapper -->
             </div> <!--.gallery-content-->
         </div><!-- #gallery-ap-primary -->

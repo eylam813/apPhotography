1c699596e0364134dc3bfdeec6e22364f000695b
Emily <lam.emily813@gmail.com>
2020-05-04 12:00:55 -0400

    Adds fallback feature image to gallery albums

--- themes/apphotography/front-page.php
+++ themes/apphotography/front-page.php
@@ -42,13 +42,19 @@ get_header();
                     ?>
                         <a class="single-album-wrapper large-4 medium-4 small-12" href="<?php echo get_permalink() ?>">
                             <div class="single-album-inner-wrapper cell">
-                                <?php the_post_thumbnail(); ?>
+                            <?php 
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail();
+                                } else { ?>
+                                    <img src="<?php bloginfo('template_directory'); ?>/assets/img/comAlexPino144.png" alt="<?php the_title(); ?>" />
+                                <?php } ?>
                             </div>
                             <h2 class="large-12 album-title"> <?php the_title(); ?></h2>
                         </a>
-                        
                         <?php endwhile; ?>
                         <!-- end of loop -->
+                        <?php else : ?>
+                            <p><?php esc_html__('') ?></p>
                         <?php endif; ?>
                     <div> <!-- #gallery-albums-inner-wrapper -->
                 </div> <!--#gallery-albums-wrapper -->
